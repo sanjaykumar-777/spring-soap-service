5af371cb08c95a3f97cf43a23e08295689387a5c
sanjay-kumar777 <123931055+sanjay-kumar777@users.noreply.github.com>
2024-08-19 22:29:39 +0530

    xml request and response structure

---
+++ src/test/java/com/sanjaykumar777/soap/webservices/soap_course_management/SoapCourseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjaykumar777.soap.webservices.soap_course_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapCourseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
